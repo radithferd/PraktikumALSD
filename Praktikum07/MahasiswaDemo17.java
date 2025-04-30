a48a502a7399a95fc19c7085f138e507df98fe7a
radithferd <radithferd@gmail.com>
2025-04-30 22:04:31 +0700

    Percobaan 2

--- Praktikum07/MahasiswaDemo17.java
+++ Praktikum07/MahasiswaDemo17.java
@@ -42,6 +42,8 @@ public class MahasiswaDemo17 {
                         int nilai = scan.nextInt();
                         dinilai.tugasDinilai(nilai);
                         System.out.printf("Nilai Tugas %s adalah %d\n", dinilai.nama, nilai);
+                        String biner = stack.konversiDesimalKeBiner(nilai);
+                        System.out.println("Nilai Biner Tugas: " + biner);
                     }
                     break;
         
--- Praktikum07/StackTugasMahasiswa17.java
+++ Praktikum07/StackTugasMahasiswa17.java
@@ -75,4 +75,18 @@ public class StackTugasMahasiswa17 {
     public int banyakTugas() {
         return top + 1;
     }
+
+    public String konversiDesimalKeBiner(int nilai) {
+        StackKonversi17 stack = new StackKonversi17(nilai);
+        while (nilai > 0) {
+            int sisa = nilai % 2;
+            stack.push(sisa);
+            nilai = nilai / 2;
+        }
+        String biner = new String();
+        while (!stack.isEmpty()) {
+            biner += stack.pop();
+        }
+        return biner;
+    } 
 }
